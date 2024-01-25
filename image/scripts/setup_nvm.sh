b26e33538f684fcfe3610eec5bc84fb7c895943c
Quentin Herzig <quentin@e-potek.ch>
2024-01-25 16:14:11 +0100

    Undo last commit

--- image/scripts/setup_nvm.sh
+++ image/scripts/setup_nvm.sh
@@ -21,14 +21,14 @@ echo "Node: $NODE_VERSION (parsed: $MAJOR_NODE_VERSION.$MINOR_NODE_VERSION.$PATC
 if [[ $MAJOR_NODE_VERSION == "14" && $MINOR_NODE_VERSION -ge 21 && $PATCH_NODE_VERSION -ge 4 ]]; then
   NODE_INSTALL_PATH="/home/app/.nvm/versions/node/v$NODE_VERSION"
 
-  if [ -f $NODE_INSTALL_PATH ]; then
+  if [ -d $NODE_INSTALL_PATH ]; then
     echo "Meteor's custom v14 LTS Node version is already installed ($NODE_VERSION)"
   else
     echo "Using Meteor's custom NodeJS v14 LTS version"
 
     # https://hub.docker.com/layers/meteor/node/14.21.4/images/sha256-f4e19b4169ff617118f78866c2ffe392a7ef44d4e30f2f9fc31eef2c35ceebf3?context=explore
     curl "https://static.meteor.com/dev-bundle-node-os/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" | tar xzf - -C /tmp/
-    mv /tmp/node-v$NODE_VERSION-linux-x64 $NODE_INSTALL_PATH --force
+    mv /tmp/node-v$NODE_VERSION-linux-x64 $NODE_INSTALL_PATH
   fi
 
   nvm use $NODE_VERSION
